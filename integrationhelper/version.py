88d7a5263a9118c204ec40dbb9ddac55df4fe7c3
ludeeus <joasoe@gmail.com>
2019-07-22 15:38:50 +0000

    update

--- integrationhelper/version.py
+++ integrationhelper/version.py
@@ -16,11 +16,31 @@ properties:
 class Version:
     """Version."""
 
-    major = 0
-    minor = 0
-    patch = 0
+    def __init__(self, major=None, minor=None, patch=None):
+        """
+        Initialize.
+
+        Usage:
+        from integrationhelper.version import Version
+        myversion = Version(1, 2, 3)
+        myversion = Version(1, 2)
+        myversion = Version(1)
+
+        print(myversion.version)
+        """
+        self.major = major
+        self.minor = minor
+        self.patch = patch
 
     @property
     def version(self):
         """Return set version as a string."""
-        return f"{self.major}.{self.minor}.{self.patch}"
+        version = []
+        if self.major is not None:
+            version.append(self.major)
+        if self.minor is not None:
+            version.append(self.minor)
+        if self.patch is not None:
+            version.append(self.patch)
+
+        return ".".join(version)
