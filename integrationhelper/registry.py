856a0f3067b64323793a1e9254ccdec24df71a9a
ludeeus <joasoe@gmail.com>
2019-07-23 06:48:44 +0000

    Add list

--- integrationhelper/registry.py
+++ integrationhelper/registry.py
@@ -1,8 +1,8 @@
 """Registry."""
 
 
-class Registry:
-    """Registry."""
+class RegistryDict:
+    """Registry dict."""
 
     register = {}
 
@@ -14,3 +14,18 @@ class Registry:
         """Remove from Registry."""
         if key in self.register:
             del self.register[key]
+
+
+class RegistryList:
+    """Registry list."""
+
+    register = []
+
+    def add(self, value):
+        """Add to the Registry."""
+        self.register.append(value)
+
+    def remove(self, value):
+        """Remove from Registry."""
+        if value in self.register:
+            self.register.remove(value)
