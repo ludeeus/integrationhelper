79c0861ec4334bb6835539acd11f561179e76907
ludeeus <joasoe@gmail.com>
2019-07-23 06:49:16 +0000

    add registries

--- integrationhelper/__init__.py
+++ integrationhelper/__init__.py
@@ -3,4 +3,5 @@ from integrationhelper.validate import Validate
 from integrationhelper.version import Version
 from integrationhelper.logger import Logger
 from integrationhelper.webclient import WebClient
-from integrationhelper.registry import Registry
+from integrationhelper.registry import RegistryDict
+from integrationhelper.registry import RegistryList
