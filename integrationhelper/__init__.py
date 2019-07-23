3215b34461a3efa3ad9d0689d69fd9e2aac71ae5
ludeeus <joasoe@gmail.com>
2019-07-23 10:53:12 +0200

    Add Throttle

--- integrationhelper/__init__.py
+++ integrationhelper/__init__.py
@@ -5,3 +5,4 @@ from integrationhelper.logger import Logger
 from integrationhelper.webclient import WebClient
 from integrationhelper.registry import RegistryDict
 from integrationhelper.registry import RegistryList
+from integrationhelper.throttle import Throttle
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="integrationhelper",
-    version="0.1.0",
+    version="0.2.0",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="A set of helpers for integrations.",
